6a8c50609a5d2370054012d9b77f284d79991524
Erik Risinger <erikrisinger@Eriks-MacBook-Pro.local>
2015-04-25 20:49:35 -0400

    Version 1.0
    Functional beta.  Not much to look at, but works as expected.

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Pegg-Timer",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0xA7, 0xDB, 0xD6, 0x1A, 0x06, 0x69, 0x46, 0x6D, 0x99, 0xC7, 0x56, 0x88, 0x12, 0x97, 0x7A, 0xE5 },
+  .virtual_size = 0xb6b6
+};
--- src/Pegg-Timer.c
+++ src/Pegg-Timer.c
@@ -1,18 +1,107 @@
 #include <pebble.h>
 
+//middle button: short press -- start/pause, long press -- restart
+//upper button: short press -- minutes++, long press -- minutes += 0.5
+//lower button: short press -- minutes--, long press -- minutes -= 0.5
+//implicit: back button to clear
+
+//future: multiple timers available through scrollable list
+
+//functions
+static void display_time();
+static void timer_handler();
+static void set_timer();
+static void start_stop_timer();
+//static void reset_timer();
+static void add_time(int delta);
+static void timer_complete();
+static void vibrate();
+
+//variables
+static AppTimer *timer;
+//static double minutes;
+static int remaining = 0;
+
 static Window *window;
 static TextLayer *text_layer;
 
+bool running = false;
+
+static void display_time(){
+	char rem[64];
+	int minutes = remaining  / 1000 / 60;
+	int seconds = (remaining - minutes * 1000 * 60) / 1000;
+	snprintf(rem, 64, "%02d: %02d", minutes, seconds);
+	text_layer_set_text(text_layer, rem);
+}
+
+static void timer_handler(){
+	if (!running) {
+		return;
+	}
+	remaining -= 1000;
+	if (remaining > 0) {
+		set_timer();
+		display_time();
+	}
+	else {
+		timer_complete();
+	}
+	
+}
+
+static void set_timer(){
+	if(running){
+		timer = app_timer_register(1000, timer_handler, NULL);
+	}
+}
+
+static void start_stop_timer(){
+	running = !running;
+	set_timer();
+}
+
+static void add_time(int delta_milliseconds){
+	remaining += delta_milliseconds;
+}
+
+static void timer_complete(){
+	//buzz, display, the whole nine
+	running = false;
+	text_layer_set_text(text_layer, "Time up!");
+	vibrate();
+	display_time();
+}
+
+static void vibrate(){
+	// Vibe pattern: ON for 200ms, OFF for 100ms, ON for 400ms:
+	static const uint32_t const segments[] = { 100, 100, 100, 500, 100, 100, 100, 500, 100, 100, 100, 500, 100, 100, 100 };
+	VibePattern pat = {
+	  .durations = segments,
+	  .num_segments = ARRAY_LENGTH(segments),
+	};
+	for (int i = 0; i < 4; i++) {
+		vibes_enqueue_custom_pattern(pat);
+	}	
+}
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+	//start/pause the timer
+	start_stop_timer();
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+	add_time(1000 * 60);
+	
+	display_time();
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+	if (remaining > 500 * 60) {
+		add_time(-500 * 60);
+	}
+	
+	display_time();
 }
 
 static void click_config_provider(void *context) {
@@ -25,9 +114,10 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, bounds.size.h } });
+  text_layer_set_text(text_layer, "00: 00");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
@@ -44,6 +134,8 @@ static void init(void) {
   });
   const bool animated = true;
   window_stack_push(window, animated);
+  set_timer();
+  
 }
 
 static void deinit(void) {
@@ -52,7 +144,7 @@ static void deinit(void) {
 
 int main(void) {
   init();
-
+  
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
   app_event_loop();
