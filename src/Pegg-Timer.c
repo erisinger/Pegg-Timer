e0f7541906b630c47751a00c5c91ccac33566f71
Erik Risinger <erikrisinger@Eriks-MacBook-Pro.local>
2015-04-28 17:05:47 -0400

    Version 1.0.1
    Added missing click-and-hold to add/subtract time.

--- src/Pegg-Timer.c
+++ src/Pegg-Timer.c
@@ -107,8 +107,8 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, 200, up_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 200, down_click_handler);
 }
 
 static void window_load(Window *window) {
